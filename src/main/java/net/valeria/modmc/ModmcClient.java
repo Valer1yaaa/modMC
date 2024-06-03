d3e81824ff5ca356282050186f9a58898f0f8601
Valer1yaaa <valeria.03gar@mail.ru>
2024-06-03 08:42:23 +0300

    start

---
+++ src/main/java/net/valeria/modmc/ModmcClient.java
@@ -0,0 +1,10 @@
+package net.valeria.modmc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModmcClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/valeria/modmc/ModmcDataGenerator.java
@@ -0,0 +1,11 @@
+package net.valeria.modmc;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ModmcDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
