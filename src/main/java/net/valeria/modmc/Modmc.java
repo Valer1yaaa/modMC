90056a996f43cc75ff9635cf0357120a39ccc14e
Valer1yaaa <valeria.03gar@mail.ru>
2024-06-03 10:02:45 +0300

    + cupcake

--- src/main/java/net/valeria/modmc/Modmc.java
+++ src/main/java/net/valeria/modmc/Modmc.java
@@ -2,6 +2,7 @@ package net.valeria.modmc;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.valeria.modmc.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class Modmc implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/valeria/modmc/item/ModItems.java
@@ -0,0 +1,30 @@
+package net.valeria.modmc.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.impl.itemgroup.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
+import net.valeria.modmc.Modmc;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registry;
+
+public class ModItems {
+
+    public static final Item CUPCAKE = registerItem("cupcake", new Item(new FabricItemSettings()));
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add(CUPCAKE);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(Modmc.MOD_ID, name), item);
+    }
+
+    public static void  registerModItems() {
+        Modmc.LOGGER.info("Registering mod items for " + Modmc.MOD_ID);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
+    }
+}
